88b014ec44c0dfbfbce9b100351d26257749179f
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-19 19:06:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 function submitData(username, email) {
- let configObj = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify(formData)
-};
+  
  
  
   fetch("http://localhost:3000/users", configObj)
