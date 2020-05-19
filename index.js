c6a7adc2c13db774b82cc48b7113afd8ccc2da30
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-19 19:13:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function submitData(name, email) {
   })
   };
  
-fetch("http://localhost:3000/users", configObj)
+return fetch("http://localhost:3000/users", configObj)
   .then(function(response) {
     return response.json();
   })
