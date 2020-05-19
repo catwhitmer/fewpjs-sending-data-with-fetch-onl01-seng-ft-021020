00a91a7cfd5a907bb40a1a64bbe209dc87fea4c6
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-19 19:20:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,13 +13,22 @@ function submitData(name, email) {
     body: JSON.stringify(formData)
   };
  
-  return fetch("http://localhost:3000/users", configurationObject).then(function(response) {
-        return response.json();
+  return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
     })
-    .then(function (object) {
-        document.body.innerHTML = object[ "id" ]
-    })
-    .catch(function (error) {
-        document.body.innerHTML = error.message
-    })
+    .then(function(object) {
+      let h2 = document.createElement('h2');
+      h2.innerHTML = object.id;
+      document.body.appendChild(h2);
+      
+      console.log(object);
+      })
+      .catch(function(error) {
+        let h3 = document.createElement('h3');
+        h3.innerHTML = error.message;
+        document.body.appendChild(h3);
+        
+        console.log(error.message);
+      });
 }
